72be6d7a95e4318bceae63e15f4f8f1c87e73970
Roel Dev <roeldev@users.noreply.github.com>
2015-03-26 21:46:37 +0100

    Core structure + util functions are working properly

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var Gulp        = require('gulp'),
-    GulpPlugins = require('gulp-load-plugins')(),
     Maelstrom   = require('./lib/index.js'),
     Del         = require('del');
 
-Maelstrom.init();
+Maelstrom.init(Gulp,
+{
+    'src':
+    {
+        'js':   './tests/input',
+        'sass': './tests/input'
+    },
+
+    'dest':
+    {
+        'css': './tests/output',
+        'js':  './tests/output'
+    }
+});
 
 Gulp.task('test:css', function()
 {
@@ -14,14 +26,16 @@ Gulp.task('test:css', function()
 
 Gulp.task('test:sass', function()
 {
-    Del('./tests/output/*.*');
+    Del(Maelstrom.sass.dest() +'/*.*');
 
-    var $src = Gulp.src('./tests/input/*.scss')
+    var $src = Gulp.src( Maelstrom.sass.src() )
         .pipe( Maelstrom.sass() )
-        .pipe( Gulp.dest('./tests/output/') );
+        .pipe( Gulp.dest(Maelstrom.sass.dest()) );
 });
 
 Gulp.task('watch:tests', function()
 {
     Gulp.watch('tests/input/*.scss', ['test:sass']);
+
+    Maelstrom.sass.watch();
 });
