7ca6cb9f69efdd48744c1b852c81308aa5b1e094
Roel Dev <roeldev@users.noreply.github.com>
2015-04-01 20:18:52 +0200

    Cleaned up a bit more.

--- examples/example2/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp      = require('gulp'),
-    maelstrom = require('maelstrom');
-
-maelstrom.requireDir('./gulp_tasks/');
-maelstrom.init();
-
-//------------------------------------------------------------------------------
-
