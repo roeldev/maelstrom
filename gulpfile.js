926b95591c3c2c4bb128abcf5491f91e4bc5f207
Roel Dev <roeldev@users.noreply.github.com>
2015-04-22 12:17:52 +0200

    Add jscs code style lint support

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 /**
  * maelstrom | gulpfile.js
- * file version: 0.00.003
+ * file version: 0.00.004
  */
 'use strict';
 
-var Maelstrom   = require('./lib/index.js'),
-    Gulp        = require('gulp'),
-    GulpJsHint  = require('gulp-jshint'),
-    GulpMocha   = require('gulp-mocha'),
-    GulpSize    = require('gulp-size'),
-    Delete      = require('del'),
-    RunSequence = require('run-sequence');
+var Maelstrom       = require('./lib/index.js');
+var Gulp            = require('gulp');
+var GulpJsCs        = require('gulp-jscs');
+var GulpJsCsStylish = require('gulp-jscs-stylish');
+var GulpJsHint      = require('gulp-jshint');
+var GulpMocha       = require('gulp-mocha');
+var GulpSize        = require('gulp-size');
+var Delete          = require('del');
+var RunSequence     = require('run-sequence');
 
 var JS_SRC = ['gulpfile.js', 'lib/**/*.js', '!lib/templates/*.js', 'test/*.js'];
 
@@ -51,8 +53,8 @@ Maelstrom.extend('customPlugin3', {});*/
  */
 Gulp.task('test:icons', function()
 {
-    var $src  = 'tests/icons/*.svg',
-        $dest = 'tests/icons/dest',
+    var $src  = 'tests/icons/*.svg';
+    var $dest = 'tests/icons/dest';
 
     $templateConfig =
     {
@@ -75,9 +77,9 @@ Gulp.task('test:icons', function()
  */
 Gulp.task('test:images', function()
 {
-    var $imgExt = Maelstrom.config.imageExtensions.join(','),
-        $src    = 'tests/images/*.{'+ $imgExt +'}',
-        $dest   = 'tests/images/dest';
+    var $imgExt = Maelstrom.config.imageExtensions.join(',');
+    var $src    = 'tests/images/*.{'+ $imgExt +'}';
+    var $dest   = 'tests/images/dest';
 
     Delete($dest +'/*.*');
 
@@ -96,8 +98,8 @@ Gulp.task('test:images', function()
  */
 Gulp.task('test:sass', function()
 {
-    var $src  = 'tests/sass/*.scss',
-        $dest = 'tests/sass/dest';
+    var $src  = 'tests/sass/*.scss';
+    var $dest = 'tests/sass/dest';
 
     Delete($dest +'/*.*');
 
@@ -115,26 +117,29 @@ Gulp.task('lint', function()
 {
     return Gulp.src(JS_SRC)
         .pipe( GulpJsHint() )
+        .pipe( GulpJsCs() ).on('error', Maelstrom.utils.noop)
+        .pipe( GulpJsCsStylish.combineWithHintResults() )
         .pipe( GulpJsHint.reporter('jshint-stylish') );
 });
 
 Gulp.task('test', function()
 {
     return Gulp.src('test/*.js', { 'read': false })
-        .pipe( GulpMocha() );
+        .pipe( GulpMocha({ 'reporter': 'spec' }) );
 });
 
 Gulp.task('dev', function()
 {
-    for(var $i = 0; $i < 30; $i++)
-    {
-        console.log('');
-    }
-
-    RunSequence('lint', 'test');
+    process.stdout.write('\u001b[2J');
+    RunSequence('test', 'lint');
 });
 
 Gulp.task('watch', function()
 {
     Gulp.watch(JS_SRC, ['dev']);
+});
+
+Gulp.task('watch:lint', function()
+{
+    Gulp.watch(JS_SRC, ['lint']);
 });
