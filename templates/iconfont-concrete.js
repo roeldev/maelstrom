934bafaa1849cdbe65777ff1524e83b5473ce511
Roel Dev <roeldev@users.noreply.github.com>
2015-07-16 16:23:00 +0200

    Iconfont related updates

--- templates/iconfont-concrete.js
+++ templates/iconfont-concrete.js
@@ -1,11 +1,19 @@
 @import '<%= importFile %>';
 
+// scss-lint:disable all
 @include iconfont('<%= fontName %>', '<%= className %>', '<%= fontPath %>')
 {
-<% _.each(glyphs, function(glyph) { %>
-    .<%= className %>-<%= glyph.name %>:before
-    {
-        content: '\<%= glyph.content %>';
-    }
-<% }); %>
+    <% _.each(glyphs, function(glyph) { %>
+
+        %<%= glyph.className %>
+        {
+            content: '\<%= glyph.content %>';
+        }
+        .<%= glyph.className %>:before
+        {
+            @extend %<%= glyph.className %>;
+        }
+
+    <% }); %>
 }
+// scss-lint:enable
